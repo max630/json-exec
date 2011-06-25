c945fd6690540bf950233b5efe880f957c45afb2
Max Kirillov <max@max630.net>
2011-06-26 00:15:53 +0300

    many changes including newConnection

--- RPC.hs
+++ RPC.hs
@@ -3,30 +3,32 @@
 module RPC where
 
 import Data.ByteString (ByteString, hPutStr)
-import Data.Aeson (FromJSON, fromJSON, ToJSON, toJSON, Value(Array, Object, Number, Null), object, (.=), Result(Success, Error), json)
+import Data.Aeson (FromJSON, fromJSON, ToJSON, toJSON, Value(Array, Object, Number, Null, String), object, (.=), Result(Success, Error), json)
 import Data.Attoparsec (parseWith)
 import qualified Data.Attoparsec as AP
 import Data.Attoparsec.Number (Number(I))
 import Data.Aeson.Encode (fromValue)
+import qualified Data.Text as T
 import Blaze.ByteString.Builder (toByteString)
 import Data.Vector (singleton)
 import Data.HashTable (insert, delete)
 import qualified Data.Map as M
 import qualified Data.HashTable as H
-import Control.Concurrent.MVar (newEmptyMVar, putMVar, takeMVar)
+import Control.Concurrent.MVar (newEmptyMVar, newMVar, putMVar, takeMVar, modifyMVar)
 import Control.Exception (bracket_)
+import Control.Concurrent (forkIO)
+import Data.Hashable (hash)
 
-data Connection a b = Connection { conn_write :: ByteString -> IO (),
-                                  conn_read :: IO ByteString,
+data Connection a b = Connection { conn_send :: Value -> IO (),
                                   conn_pending :: a,
                                   conn_counter :: IO Integer,
-                                  conn_handlers :: b}
+                                  conn_methods :: b}
 
 data Handler = forall a b . (FromJSON a, ToJSON b) => Handler (a -> IO b)
 
 
 -- registerMethodHandler :: (FromJSON a, ToJSON b) => Connection -> String -> (a -> IO b) -> IO ()
-registerMethodHandler conn name handler = insert (conn_handlers conn) name (Handler handler)
+registerMethodHandler conn name handler = insert (conn_methods conn) name (Handler handler)
 
 mlookup ks kx = if all (flip M.member kx) ks then Just (map ((M.!) kx) ks) else Nothing
 
@@ -41,29 +43,62 @@ getMethod conn name = return f
           (insert (conn_pending conn) name (putMVar var))
           (delete (conn_pending conn) name)
           (do 
-            conn_write conn (toByteString $ fromValue $ object ["id" .= id, "method" .= name, "params" .= singleton (toJSON a)])
+            conn_send conn (object ["id" .= id, "method" .= name, "params" .= singleton (toJSON a)])
             response <- takeMVar var
             case fromJSON response of
               Success res -> return res
               Error err -> fail err)
 
-      
 
-handleInputs conn = loop ""
-  where
-    loop buffer =
-      do
-        result <- parseWith (conn_read conn) json buffer
-        case result of
-          AP.Fail _ _ msg -> fail ("failed to parse input: " ++ msg)
-          AP.Partial _ -> fail "Partial must not be here"
-          AP.Done newBuffer message ->
-            do
-              dispatch conn message
-              loop newBuffer
+newConnection readF writeF =
+  do
+    pending <- H.new (==) (fromInteger . toInteger . hash)
+    methods <- H.new (==) (fromInteger . toInteger . hash)
+    writeVar <- newEmptyMVar
+    counter <- newMVar 0
+    let
+      writer =
+        do
+          value <- takeMVar writeVar
+          writeF (toByteString $ fromValue value)
+          writer
+      reader buffer =
+        do
+          parseResult <- parseWith readF json buffer
+          case parseResult of
+            AP.Fail _ _ msg -> fail ("failed to parse input: " ++ msg)
+            AP.Partial _ -> fail "Partial must not be here"
+            AP.Done newBuffer message ->
+              do
+                dispatch conn message
+                reader newBuffer
+      conn = Connection {
+              conn_send = (putMVar writeVar),
+              conn_pending = pending,
+              conn_counter = modifyMVar counter (\v -> return (v + 1, v)),
+              conn_methods = methods
+              }
+    forkIO (reader "")
+    forkIO writer
+    return conn
 
 dispatch conn (Object (mlookup ["id", "method", "params"] -> Just [Null, method, params])) = undefined -- TODO: notifications
-dispatch conn (Object (mlookup ["id", "method", "params"] -> Just [id, method, params])) = undefined -- TODO: implement lookup in conn
+dispatch conn (Object (mlookup ["id", "method", "params"] -> Just [id, String name, params])) =
+  do
+    handlerMb <- H.lookup (conn_methods conn) name
+    case handlerMb of
+      Just handler ->
+        do
+          response <- catch
+                      (do
+                        res <- handler (fromJSON params)
+                        return (object ["id" .= id, "error" .= Null, "result" .= toJSON res]))
+                      (\err -> return (errorResponse $ show err))
+          conn_send conn response
+      Nothing -> conn_send conn (errorResponse ("Unknown method: " ++ T.unpack name))
+  where
+    errorResponse errorString = object ["id" .= id, "error" .= errorString, "result" .= Null]
+
 dispatch conn o@(Object (mlookup ["id", "response", "error"] -> Just [id, _, _])) =
   do
     handler <- H.lookup (conn_pending conn) id
