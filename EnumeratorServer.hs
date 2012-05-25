6c8e1ffcd32f6bbdf29b396d057d53abae0da72b
Max Kirillov <max@max630.net>
2012-05-25 16:56:17 +0300

    start digging into Enumerator

---
+++ EnumeratorServer.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+module EnumeratorServer where
+
+import Data.Attoparsec.Enumerator (iterParser)
+import Data.Aeson.Parser (json)
+import Data.Enumerator (run_)
+import Data.Enumerator.Binary (enumHandle)
+import GHC.IO.Handle.FD (stdin, stdout)
+import Control.Monad.IO.Class (MonadIO(liftIO))
+import Data.Enumerator (($$), printChunks)
+
+iter = iterParser json
+
+-- blindly echoing chinks
+echo0 = run_ ((enumHandle 10240 stdin) $$ (printChunks True))
