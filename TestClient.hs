a699a40b91cdf730f05a0fc6e327cefcc170ec23
Max Kirillov <max@max630.net>
2012-05-29 17:58:26 +0300

    used the new interface

--- TestClient.hs
+++ TestClient.hs
@@ -9,7 +9,7 @@ makeConnection = RPC.newConnectionCommand False (RawCommand "./TestServer" [])
 main =
   do
     c <- makeConnection
-    mIO <- RPC.getMethod c "exp" :: IO (Double -> IO Double)
+    let mIO = RPC.callMethod c "exp" :: Double -> IO Double
     let m v = unsafePerformIO (mIO v)
     putStrLn ("m 1 = " ++ show (m 1))
     putStrLn ("m 10 = " ++ show (m 10))
